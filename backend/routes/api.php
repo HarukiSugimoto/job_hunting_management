3e3671c9aac28e0452018787fef11cbb0ed1d7d3
HarukiSugimoto <sugisugiharu@gmail.com>
2025-05-13 13:18:58 +0900

    move: controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('companies', 'App\Http\Controllers\CompanyController');
+    Route::apiResource('my_pages', 'App\Http\Controllers\MyPageController');
+    Route::apiResource('deadlines', 'App\Http\Controllers\DeadlineController');
+});
