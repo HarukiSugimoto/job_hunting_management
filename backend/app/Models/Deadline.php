6c5ca07d6368f53750e486eab53631440b6d47f4
HarukiSugimoto <sugisugiharu@gmail.com>
2025-05-13 13:15:20 +0900

    add: model

--- backend/app/Models/Deadline.php
+++ backend/app/Models/Deadline.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Deadline extends Model
 {
-    //
+    protected $fillable = [
+        'date',
+        'type',
+        'status',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- backend/app/Models/MyPage.php
+++ backend/app/Models/MyPage.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class MyPage extends Model
 {
-    //
+    protected $fillable = [
+        'date',
+        'type',
+        'status',
+    ];
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
